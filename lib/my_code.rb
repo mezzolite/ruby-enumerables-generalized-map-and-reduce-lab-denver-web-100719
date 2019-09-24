104f38b7be99a1b7f32f20abace235c33978d84d
Mez Charney <mez.charney@gmail.com>
2019-09-24 17:35:20 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
-# Your Code Here
+def map(array)
+new = []
+counter = 0
+while counter < array.length do
+  new.push(yield(array[counter]))
+  counter += 1
+end
+new
+end
+
+def reduce(array, starting_value = nil)
+ if starting_value
+  sum = starting_value
+  counter = 0 
+else
+  sum = array[0]
+  counter = 1 
+end
+  
+  while counter < array.length do
+    sum = yield(sum, array[counter])
+    counter += 1 
+  end 
+  sum
+end
